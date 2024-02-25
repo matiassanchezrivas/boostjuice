20b16f42cd5d074e2f26f06a7e9cfb3842b6252b
Matías Sánchez Rivas <matiassanchezrivas@gmail.com>
2024-02-25 16:24:34 +1100

    Configure aliases

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,20 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    [
+      'babel-plugin-module-resolver',
+      {
+        root: ['./src'],
+        extensions: ['.android.js', '.ios.js', '.js', '.json', '.ts', '.tsx'],
+        alias: {
+          '@components': './src/components',
+          '@graphql': './src/graphql',
+          '@queries': './src/queries',
+          '@screens': './src/screens',
+          '@theme': './src/theme',
+          '@utils': './src/utils',
+        },
+      },
+    ],
+  ],
 };
